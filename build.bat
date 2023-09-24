9408f50ccace71a4265f49855b49810ed37015bd
chen <3217855084@qq.com>
2023-09-24 19:52:01 +0800

    add Windows build

---
+++ build.bat
@@ -0,0 +1,8 @@
+if not exist build (
+    mkdir build
+)
+cd build
+cmake -DCMAKE_BUILD_TYPE="Release" ..
+MSBuild.exe .\ALL_BUILD.vcxproj -p:Configuration=Release
+cd ..
+mv .\build\Release\hmac.exe .\hmac.exe
